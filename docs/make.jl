defe5058cf3974de1e7a40f4125e5c5374a9b634
Charles Troupin <charles.troupin@gmail.com>
2023-12-12 14:18:13 +0100

    deploy doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,17 @@
 push!(LOAD_PATH,"../src/")
 using Documenter
 
-makedocs(sitename="My Documentation")
+makedocs(;
+    format=Documenter.HTML(),
+    pages=[
+        "Home" => "index.md",
+    ],
+    repo="https://github.com/gher-uliege/MVRE-DIVAnd/blob/{commit}{path}#L{line}",
+    sitename="MVRE DIVAnd documentation",
+    authors="Charles Troupin <ctroupin@uliege.be>",
+    checkdocs=:none,
+)
+
+deploydocs(;
+    repo="github.com/gher-uliege/MVRE-DIVAnd",
+)
